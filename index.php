fa90dd628140111b816e473ee69a4a5248bd4529
unknown <emanuel-souza1@hotmail.com>
2019-07-30 01:04:57 -0300

    métodos de lista

--- index.php
+++ index.php
@@ -2,9 +2,31 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+// //Carrega um usuário
 
-$root -> loadbyid(5);
+// $root = new Usuario();
 
-echo $root;
+// $root -> loadbyid(5);
+
+// echo $root;
+
+
+// //Carrega uma lista de usuários
+// $lista = Usuario::getList();
+
+// echo json_encode($lista);
+
+// //Carrega uma lista de usuários buscando pelo login
+
+// $search = Usuario::search('funcionaporfavor');
+
+// echo json_encode($search);
+
+//Carrega um usuario usando login e senha
+
+$usuario = new Usuario();
+
+$usuario -> login("teste","testando");
+
+echo $usuario;
 ?>
